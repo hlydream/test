694e6f5d98ef37ffb44e446366ab365f7c847e81
hlydream <1605104939@qq.com>
2016-08-24 06:39:30 +0800

    2.1
    测试加密，添加登录功能

--- index.php
+++ index.php
@@ -26,4 +26,33 @@ BNyRyWchglQevmFSwDM1mz0dkFUle/TSJ4pFSPzNUWDeSo4QNoWmtJkT1MMjM1rZ
 	$pi_key =  openssl_pkey_get_private($private_key);//这个函数可用来判断私钥是否是可用的，可用返回资源id Resource id  
 	$pu_key = openssl_pkey_get_public($public_key);//这个函数可用来判断公钥是否是可用的  
 	print_r($pi_key);echo "\n";  
-	print_r($pu_key);echo "\n";  
+	print_r($pu_key);echo "\n";
+
+	$data = "nokia1320";//要加密的原始数据  
+	$encrypted = "";  //已加密的数据 
+	$decrypted = "";   //解密后的原始数据
+	  
+	echo "source data:",$data,"\n";  
+	  
+	echo "private key encrypt:\n";  
+	  
+	openssl_private_encrypt($data,$encrypted,$pi_key);//私钥加密  
+	$encrypted = base64_encode($encrypted);//加密后的内容通常含有特殊字符，需要编码转换下，在网络间通过url传输时要注意base64编码是否是url安全的  
+	echo $encrypted,"\n";  
+	  
+	echo "public key decrypt:\n";  
+	  
+	openssl_public_decrypt(base64_decode($encrypted),$decrypted,$pu_key);//私钥加密的内容通过公钥可用解密出来  
+	echo $decrypted,"\n";  
+	  
+	echo "---------------------------------------\n";  
+	echo "public key encrypt:\n";  
+	  
+	openssl_public_encrypt($data,$encrypted,$pu_key);//公钥加密  
+	$encrypted = base64_encode($encrypted);  
+	echo $encrypted,"\n";  
+  
+	echo "private key decrypt:\n";  
+	openssl_private_decrypt(base64_decode($encrypted),$decrypted,$pi_key);//私钥解密  
+	echo $decrypted,"\n";  
+	
