5685a30fd5dd7949c8ce8aa7c293d8587ef3abb9
hlydream <1605104939@qq.com>
2016-08-24 07:08:43 +0800

    2.4
    登录功能测试

--- login.php
+++ login.php
@@ -1,2 +1,19 @@
 <?php
-	
+		$data=array(
+			'name'=>'root',
+			'password'=>'nokia1320',
+			'aim'=>'login'
+		);
+		//准备post的数据
+	    $postdata = http_build_query($data); 
+		//数据头
+        $opts = array('http' =>
+                      array( 
+                          'method'  => 'POST', 
+                          'header'  => 'Content-type: application/x-www-form-urlencoded', 
+                          'content' => $postdata
+                      )
+        );
+        $context = stream_context_create($opts);
+        $result = file_get_contents($url, false, $context);
+		print_r($result);
